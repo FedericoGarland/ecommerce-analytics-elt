08a0d286e0f7ed8b2d23b60810da87ea71af2b5a
Federico Garland <federicogarlands@gmail.com>
2026-02-14 14:21:10 -0500

    Created sources.yml for staging area

---
+++ code/dbt/ecommerce_dbt/models/staging/stg_customers.sql
@@ -0,0 +1,22 @@
+{{ config(materialized='table') }}
+
+with source as (
+
+    select * 
+    from {{ source('raw', 'OLIST_CUSTOMERS') }}
+
+),
+
+renamed as (
+
+    select
+        customer_id,
+        customer_unique_id,
+        customer_zip_code_prefix,
+        customer_city,
+        customer_state
+    from source
+
+)
+
+select * from renamed
