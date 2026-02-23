9ef6a20afdcb7600a131c612fc557dfcca53eb83
Federico Garland <federicogarlands@gmail.com>
2026-02-22 23:48:51 -0500

    Completed first succesful Prefect pipeline to ingest and dbt transform the customers table

--- code/orchestration/prefect/customers_flow.py
+++ code/orchestration/prefect/customers_flow.py
@@ -10,6 +10,43 @@ DBT_PROJECT_DIR = r"C:\Users\User\Documents\ecommerce-analytics-elt\code\dbt\eco
 
 
 @task
+def truncate_table():
+    """
+    1) Cargar credenciales
+    2) Conectar a Snowflake
+    3) Ejecutar COPY INTO directamente
+    """
+
+    load_dotenv()
+
+    conn = snowflake.connector.connect(
+        account=os.getenv("SNOWFLAKE_ACCOUNT"),
+        user=os.getenv("SNOWFLAKE_USER"),
+        password=os.getenv("SNOWFLAKE_PASSWORD"),
+        role=os.getenv("SNOWFLAKE_ROLE"),
+        warehouse=os.getenv("SNOWFLAKE_WAREHOUSE"),
+        database=os.getenv("SNOWFLAKE_DATABASE"),
+    )
+
+    cur = conn.cursor()
+
+    copy_sql = """
+    TRUNCATE TABLE ECOMMERCE.RAW.OLIST_CUSTOMERS;
+    """
+
+    cur.execute(copy_sql)
+
+    results = cur.fetchall()
+
+    print("✅ COPY INTO result:")
+    for row in results:
+        print(row)
+
+    cur.close()
+    conn.close()
+
+
+@task
 def ingest_customers():
     """
     1) Cargar credenciales
@@ -32,9 +69,10 @@ def ingest_customers():
 
     copy_sql = """
     COPY INTO ECOMMERCE.RAW.OLIST_CUSTOMERS
-    FROM @ECOMMERCE.RAW.AZURE_RAW_STAGE/customers/
+    FROM @ECOMMERCE.RAW.AZURE_RAW_STAGE/customers/ingestion_date=2025-12-06
     FILE_FORMAT = (FORMAT_NAME = ECOMMERCE.RAW.CSV_FORMAT)
-    PATTERN = '.*olist_customers_dataset\\.csv'
+    PATTERN = '.*\\.csv.*'
+    ON_ERROR = 'ABORT_STATEMENT'
     FORCE = TRUE;
     """
 
@@ -64,9 +102,10 @@ def run_dbt_staging():
 
 @flow(name="customers_pipeline_simple")
 def customers_pipeline_simple():
+    truncate_table()
     ingest_customers()
     run_dbt_staging()
 
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     customers_pipeline_simple()
